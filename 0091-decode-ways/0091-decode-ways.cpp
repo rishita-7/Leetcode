0f7fdec3966b0e793df44633276ed3b2f41fc7ff
Rishita Kishor <kishorrishita1@gmail.com>
2026-07-21 16:17:48 +0530

    Time: 2 ms (22.26%), Space: 8.5 MB (88.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n=s.size();
+        if (s[0]=='0')
+            return 0;
+        if(n==1)
+            return 1;
         vector<int>dp(n+1,0);
         dp[0]=1;
         for(int i=1;i<=n;i++){
+            //single character-> if the character is 0, dp[i] will remain 0, else the dp[prev character] will be copied
             if(s[i-1]!='0')
                 dp[i]=dp[i-1];
-
-            // 2-digit decoding:-> if previous digit=1-> any digit will work
-            // if previous digit=2 -> only 0 to 6 (20->26)
-            if(i>1 && (s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<='6'))){
+            if (i>1 &&(s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<='6')))
                 dp[i]+=dp[i-2];
-            }
         }
         return dp[n];
     }

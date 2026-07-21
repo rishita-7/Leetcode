d5cc43398439ca51d7d0b5cee2ceb508c9475c83
Rishita Kishor <kishorrishita1@gmail.com>
2026-07-21 15:57:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (80.04%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -7,6 +7,9 @@ public:
         for(int i=1;i<=n;i++){
             if(s[i-1]!='0')
                 dp[i]=dp[i-1];
+
+            // 2-digit decoding:-> if previous digit=1-> any digit will work
+            // if previous digit=2 -> only 0 to 6 (20->26)
             if(i>1 && (s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<='6'))){
                 dp[i]+=dp[i-2];
             }
